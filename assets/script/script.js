1240e8d46aa346d6e0ca42e80377ae7cbdd75acd
uji100 <ujithamigara@gmail.com>
2023-10-18 21:44:06 +0530

    add if scroller started style

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () => {
     $('#preloader').css("display", "none");
 });
+AOS.init();
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header'); // Change 'header' to match your header's HTML structure
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled-header');
+    } else {
+        header.classList.remove('scrolled-header');
+    }
+});
