f3a8dea5b4d59f76fa3b287585183a4b98c50b78
uji100 <ujithamigara@gmail.com>
2023-10-21 21:45:14 +0530

    add mob nav js

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,11 +3,22 @@ window.addEventListener("load", () => {
 });
 AOS.init();
 window.addEventListener('scroll', function() {
-    var header = document.querySelector('header'); // Change 'header' to match your header's HTML structure
+    var header = document.querySelector('header');
 
     if (window.scrollY > 0) {
         header.classList.add('scrolled-header');
     } else {
         header.classList.remove('scrolled-header');
     }
+});
+$("#nav-icon-open").on("click", () => {
+    $("#nav-icon-open").css("display", "none");
+    $("#nav-icon-close").css("display", "block");
+    $("#mob-nav").css("display", "block");
+});
+
+$("#nav-icon-close").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+    $("#mob-nav").css("display", "none");
 });
