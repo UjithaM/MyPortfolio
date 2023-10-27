c066dd7816765771464f37473094288213c55278
uji100 <ujithamigara@gmail.com>
2023-10-27 10:17:34 +0530

    add assignment 08 button action

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,3 +28,6 @@ $("#assignment-03-button").on("click", () => {
 $("#assignment-04-button").on("click", () => {
     window.location.href = "assets/assignment/assignment-04/Assignment04.html";
 });
+$("#assignment-08-button").on("click", () => {
+    window.location.href = "assets/assignment/assignment-08/Assignment08.html";
+});
