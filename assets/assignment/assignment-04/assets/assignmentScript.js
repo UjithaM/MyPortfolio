bc7ff6c6ccd71e2bf54ab387df8ae3e08a8af5fb
uji100 <ujithamigara@gmail.com>
2023-10-27 09:20:08 +0530

    add assignment 04

---
+++ assets/assignment/assignment-04/assets/assignmentScript.js
@@ -0,0 +1,23 @@
+$("#case-01-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
+
+$("#case-02-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
+
+$("#case-03-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
+
+$("#case-04-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
+
+$("#case-05-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
+
+$("#case-06-live-button").on("click", () => {
+    window.location.href = "../../../../index.html";
+});
