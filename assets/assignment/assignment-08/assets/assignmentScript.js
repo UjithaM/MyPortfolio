1da8e2c537b29dc851452cdd118258b750d0a89c
uji100 <ujithamigara@gmail.com>
2023-10-27 10:16:41 +0530

    add an assignment script with implementation

---
+++ assets/assignment/assignment-08/assets/assignmentScript.js
@@ -0,0 +1,7 @@
+$("#case-01-live-button").on("click", () => {
+    window.location.href = "../assignment-08/cases/01_case/index.html";
+});
+
+$("#case-02-live-button").on("click", () => {
+    window.location.href = "../assignment-08/cases/02_case/index.html";
+});
